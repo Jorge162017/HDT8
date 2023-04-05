1a5da377b883f68148595a543f2f0bd91f208872
jorgelopez <lop221038@uvg.edu.gt>
2023-04-04 21:00:52 -0600

    class paciente

---
+++ HT8/src/Icomparator.java
@@ -0,0 +1,3 @@
+public interface Icomparator <T> {
+    public int Compare(T abb1, T abb2);
+}
---
+++ HT8/src/Paciente.java
@@ -0,0 +1,34 @@
+public class Paciente {
+    private String nombre;
+    private String problema;
+    private String prioridad;
+
+    public Paciente(String nombre, String problema, String prioridad) {
+        this.nombre = nombre;
+        this.problema = problema;
+        this.prioridad = prioridad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getProblema() {
+        return problema;
+    }
+    public void setProblema(String problema) {
+        this.problema = problema;
+    }
+    public String getPrioridad() {
+        return prioridad;
+    }
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public int compareTo(Paciente arg0) {
+        // TODO Auto-generated method stub
+        return arg0.getPrioridad().compareTo(prioridad)*-1;
+    }
+}
