3d074fd0fea0cc50b4d1b4e891022aed908f1091
jorgelopez <lop221038@uvg.edu.gt>
2023-04-04 21:42:21 -0600

    actualizacion main

--- HT8/src/Main.java
+++ HT8/src/Main.java
@@ -1,5 +1,30 @@
+import java.io.IOException;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hoja de Trabajo 8: Sistema de colas con prioridad");
+    public static void main(String[] args)throws IOException {
+        Scanner input = new Scanner(System.in);
+        Operaciones controlador = new Operaciones();
+
+
+        System.out.println("Como quiere ordenar a los pacientes? Porfavor seleccione una opcion.");
+        System.out.println("1. PriorityQueue");
+        System.out.println("2. VectorHeap");
+        int valor = input.nextInt();
+
+        if (valor == 1) {
+            controlador.leerarchivoalistaVectorHeap("pacientes.txt");
+            System.out.println(controlador.ordenadovectorHeap());
+        }
+        else if (valor == 2) {
+            controlador.leerarchivoalistaPriorityQueue("pacientes.txt");
+            System.out.println(controlador.ordenadoPriorityQueue());
+        }
+
+        else {
+            System.out.println("Numero Incorrecto");
+
+        }
+
     }
+
 }
